019b59bad89ebe93a1f3c28cbd106ac285e1a8fd
andy_chiodi <andy.chiodi@noaa.gov>
2022-08-24 19:08:53 -0700

    fixed twdir_std, read/save from /storage/spark

--- todaily/wrfdaily_tz.py
+++ todaily/wrfdaily_tz.py
@@ -2,14 +2,14 @@ import pyferret
 import datetime
 from xy2latlon import roffset
 
-lfn = 'wrfdaily_test_logfile.txt'
+lfn = 'wrfdaily_logfile.txt'
 lfid = open(lfn,'w+')
 now = datetime.datetime.now()
 lfid.write(str(now)+'\n')
 
 s = ' '
-for i in range(160,181):            # ilon
-    for j in range(220,241):        # jlat
+for i in range(90,91):            # ilon  90,251  previously 160,181
+    for j in range(1,2):         # jlat  1,283   previously 220,241
        i1 = i-1;
        j1 = j-1;  # convert to Ferret index, which starts with 1, wheras python starts with 0
        [offset,lat,lon] = roffset(i1,j1)
